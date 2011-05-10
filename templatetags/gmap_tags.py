9dc0ccfb2be185764afc7e5957958857ed887cc4
David Pflug <david@tpflug.com>
2011-05-10 09:23:36 -0400

    Basically just adding TODO

--- templatetags/gmap_tags.py
+++ templatetags/gmap_tags.py
@@ -22,6 +22,7 @@ class GmapMarkerType(template.Node):
 def marker_types_tag(parser, token):
     try:
         tag_name, arg = token.contents.split(None, 1)
+        print(token.contents.split(None, 1))
     except ValueError:
         raise template.TemplateSyntaxError(
                 "%r tag requires arguments" % token.contents.split()[0])
@@ -30,4 +31,17 @@ def marker_types_tag(parser, token):
         raise template.TemplateSyntaxError("%r tag had invalid arguments")
     return GmapMarkerType(var_name)
 
+
+def markers_tag(parser, token):
+    try:
+        tag_name, arg = token.contents.split(None,1)
+    except ValueError:
+        raise template.TemplateSyntaxError(
+                "%r tag requires arguments" % token.contents.split()[0])
+    var_name = re.search(r'as (\w+)', arg).groups()[0]
+    if not var_name:
+        raise template.TemplateSyntaxError("%r tag had invalid arguments")
+    return GmapMarkers(var_name)
+
 register.tag('gmap_marker_types', marker_types_tag)
+register.tag('gmap_markers', markers_tag)
